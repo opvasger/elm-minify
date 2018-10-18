c60d055151f04e409c15e05370db4846ced81717
opvasger <opvasger@gmail.com>
2018-10-18 20:10:27 +0200

    testing boilerplate

---
+++ test/api.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+
+describe("the Node.js API", function () {
+
+    it("should pass", function () {
+
+        assert.strictEqual(1, 1)
+    })
+})
---
+++ test/cli.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+
+describe("the CLI", function () {
+
+    it("should pass", function () {
+
+        assert.strictEqual(1, 1)
+    })
+})
