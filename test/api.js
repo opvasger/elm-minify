5d73cba345a100c376dc6940bcf82dee85a8d7d8
opvasger <opvasger@gmail.com>
2018-11-06 03:47:13 +0100

    tests

--- test/api.js
+++ test/api.js
@@ -1,28 +1,47 @@
-var assert = require("assert")
+var is = require("assert")
+var ter = require("terser")
+var wp = require("webpack")
+
+var api = require("../src/api.js")
 
 describe("Node.js API", function () {
 
-    describe("terserConfig object", function () {
+    describe("terserConfig", function () {
 
-        it("should have tests", function () {
+        it("is a valid configuration for Terser", function () {
 
-            assert.strictEqual(true, false, "no tests yet...")
+            var result = undefined
+
+            try {
+
+                result = ter.minify("var add = function(x, y) { return x + y }", api.terserConfig)
+            }
+            catch (error) {
+
+                is.fail("Terser threw an expection during minification", error)
+            }
+
+            is.strictEqual(result.error, undefined, "The configuration was accapted, but resulted in an error")
         })
     })
 
-    describe("WebpackPlugin object", function () {
+    describe("WebpackPlugin", function () {
 
-        it("should have tests", function () {
+        it("is a valid configuration for Webpack", function () {
 
-            assert.strictEqual(true, false, "no tests yet...")
+            wp({ plugins: [new api.WebpackPlugin()] }, function (error, stats) {
+
+                is.strictEqual(error, null, "The configuration wasn't accepted by Webpack")
+            })
         })
     })
 
-    describe("minify function", function () {
+    describe("minify", function () {
 
         it("should have tests", function () {
 
-            assert.strictEqual(true, false, "no tests yet...")
+            is.strictEqual(true, false, "no tests yet...")
         })
     })
+
 })
